4123142a289a883347c7d3cdc298f9b802de24e3
Piotr Płaczek <piotr@pplaczek.pl>
2025-03-07 15:04:50 +0100

    Support terminal shotcuts for moving input cursor

--- src/app/terminal/components/terminal-command-input/terminal-command-input.component.ts
+++ src/app/terminal/components/terminal-command-input/terminal-command-input.component.ts
@@ -41,15 +41,41 @@ export class TerminalCommandInputComponent implements AfterViewInit {
         break;
 
       case 'ArrowUp':
+        $event.preventDefault();
         srcElement.value = this.commandsHistory.previous();
+        this.moveCursorAtEnd(srcElement);
         break;
+
       case 'ArrowDown':
         srcElement.value = this.commandsHistory.next();
         break;
+
+      case 'KeyA':
+        if (ctrlKey) {
+          $event.preventDefault();
+          this.moveCursorAtStart(srcElement);
+        }
+
+        break;
+
+      case 'KeyK':
+        if (ctrlKey && srcElement.selectionStart === 0) {
+          $event.preventDefault();
+          this.clearInput(srcElement);
+        }
+        break;
+
       case 'KeyL':
         if (ctrlKey) {
           $event.preventDefault();
           this.command.emit('clear');
+        }
+        break;
+
+      case 'KeyW':
+        if (ctrlKey) {
+          $event.preventDefault();
+          this.removeWordBeforeCursor(srcElement);
         }
         break;
 
@@ -64,6 +90,7 @@ export class TerminalCommandInputComponent implements AfterViewInit {
   }
 
   private submitInput(element: HTMLInputElement): void {
+    element.value = element.value.trim();
     if (!element.value) {
       return;
     }
@@ -74,5 +101,32 @@ export class TerminalCommandInputComponent implements AfterViewInit {
 
   private clearInput(element: HTMLInputElement): void {
     element.value = '';
+  }
+
+  private moveCursorAtStart(element: HTMLInputElement): void {
+    element.setSelectionRange(0, 0);
+  }
+
+  private moveCursorAtEnd(element: HTMLInputElement): void {
+    element.setSelectionRange(element.value.length, element.value.length);
+  }
+
+  private removeWordBeforeCursor(element: HTMLInputElement): void {
+    const cursorPosition = element.selectionStart!;
+    const beforeCursor = element.value.trim().substring(0, cursorPosition);
+    const separators = /[\s\/\\.,;:_-]+/;
+
+    let lastSeparatorIndex = -1;
+    for (let i = cursorPosition - 1; i > -1; i--) {
+      if (separators.test(beforeCursor[i])) {
+        lastSeparatorIndex = i;
+        break;
+      }
+    }
+
+    element.value =
+      element.value.substring(0, lastSeparatorIndex + 1) +
+      element.value.substring(cursorPosition);
+    element.setSelectionRange(lastSeparatorIndex + 1, lastSeparatorIndex + 1);
   }
 }
